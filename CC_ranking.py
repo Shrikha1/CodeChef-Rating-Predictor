868b9c8204eb2e36b105bbf7bec71059e747799c
Shrikha <mahanty.shrikha@gmail.com>
2019-06-18 16:06:06 +0530

    Contest Rankings for some contest
    Python code to access CC ranking of a given contest through API and store the rank, user_handle and name in a csv file.

---
+++ CC_ranking.py
@@ -0,0 +1,29 @@
+# Importing necessary modules
+from urllib.request import Request, urlopen   # to access data from the url 
+import urllib,json                            # to parse json data
+import pandas as pd                           # to organise data in csv format
+
+ContestID = 'JUNE19A'
+#path = 'https://www.codechef.com/api/rankings/'+ContestID
+path = 'https://www.codechef.com/api/rankings/'+ContestID+'?sortBy=score&order=desc&page=1&itemsPerPage=100&fbclid=IwAR08gwUo_Opd6La9ha1OWvVdepRGpUeZK-bBxTc9tCjNL0G6MXEQEgjk4-Q'
+req = Request(path , headers = {'User-Agent':'Mozilla/5.0'})
+webpage = urlopen(req)
+obj = json.load(webpage)
+
+rank = []
+userHandle = []
+name = []
+i = obj['list']
+
+# Extracting data
+for j in range(len(i)):
+    rank.append(i[j]['rank'])
+    userHandle.append(i[j]['user_handle'])
+    name.append(i[j]['name'])
+
+# creating a Dataframe
+df = pd.DataFrame({'user_handle':userHandle,'name':name},index = rank).sort_index()
+
+# Saving into a csv format
+df.to_csv('ContestRanking.csv')  
+
